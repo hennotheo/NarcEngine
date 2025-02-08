d33e4cef5aa0ddeef9b1da6d16cff90f27e45d7e
Nazio_LT <zoupeurletroll@gmail.com>
2025-02-08 22:21:27 +0100

    :sparkles: Add Vertex Buffer

--- NarcEngine/src/NarcEngine/Engine.h
+++ NarcEngine/src/NarcEngine/Engine.h
@@ -2,6 +2,7 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 #include <iostream>
 #include <stdexcept>
@@ -12,104 +13,110 @@
 #include <set>
 #include <cstdint>
 #include <limits>
-#include <algorithm> 
+#include <algorithm>
 #include <fstream>
 
 namespace NarcEngine
 {
-	struct QueueFamilyIndices
-	{
-		std::optional<uint32_t> GraphicsFamily;
-		std::optional<uint32_t> PresentFamily;
+    struct Vertex
+    {
+        glm::vec2 pos;
+        glm::vec3 color;
+    };
 
-		bool IsComplete()
-		{
-			return GraphicsFamily.has_value() && PresentFamily.has_value();
-		}
-	};
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> GraphicsFamily;
+        std::optional<uint32_t> PresentFamily;
 
-	struct SwapChainSupportDetails 
-	{
-		VkSurfaceCapabilitiesKHR Capabilities;
-		std::vector<VkSurfaceFormatKHR> Formats;
-		std::vector<VkPresentModeKHR> PresentModes;
-	};
+        bool IsComplete()
+        {
+            return GraphicsFamily.has_value() && PresentFamily.has_value();
+        }
+    };
 
-	class Engine
-	{
-	public:
-		void Run();
+    struct SwapChainSupportDetails
+    {
+        VkSurfaceCapabilitiesKHR Capabilities;
+        std::vector<VkSurfaceFormatKHR> Formats;
+        std::vector<VkPresentModeKHR> PresentModes;
+    };
 
-	private:
-		GLFWwindow* m_window;
-		VkInstance m_instance;
-		VkDebugUtilsMessengerEXT m_debugMessenger;
-		VkDevice m_device;
-		VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
-		VkSurfaceKHR m_surface;
+    class Engine
+    {
+    public:
+        void Run();
 
-		VkQueue m_presentQueue;
-		VkQueue m_graphicsQueue;
+    private:
+        GLFWwindow* m_window;
+        VkInstance m_instance;
+        VkDebugUtilsMessengerEXT m_debugMessenger;
+        VkDevice m_device;
+        VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
+        VkSurfaceKHR m_surface;
 
-		VkSwapchainKHR m_swapChain;
-		std::vector<VkImage> m_swapChainImages;
-		VkFormat m_swapChainImageFormat;
-		VkExtent2D m_swapChainExtent;
-		std::vector<VkImageView> m_swapChainImageViews;
-		std::vector<VkFramebuffer> m_swapChainFramebuffers;
+        VkQueue m_presentQueue;
+        VkQueue m_graphicsQueue;
 
-		VkRenderPass m_renderPass;
-		VkPipelineLayout m_pipelineLayout;
-		VkPipeline m_graphicsPipeline;
-		VkCommandPool m_commandPool;
-		std::vector<VkCommandBuffer> m_commandBuffers;
-		uint32_t m_currentFrame = 0;
+        VkSwapchainKHR m_swapChain;
+        std::vector<VkImage> m_swapChainImages;
+        VkFormat m_swapChainImageFormat;
+        VkExtent2D m_swapChainExtent;
+        std::vector<VkImageView> m_swapChainImageViews;
+        std::vector<VkFramebuffer> m_swapChainFramebuffers;
 
-		//Sync objects -> Waiting them to make operation... cf doc vulkan
-		std::vector<VkSemaphore> m_imageAvailableSemaphores;
-		std::vector<VkSemaphore> m_renderFinishedSemaphores;
-		std::vector<VkFence> m_inFlightFences;
+        VkRenderPass m_renderPass;
+        VkPipelineLayout m_pipelineLayout;
+        VkPipeline m_graphicsPipeline;
+        VkCommandPool m_commandPool;
+        std::vector<VkCommandBuffer> m_commandBuffers;
+        uint32_t m_currentFrame = 0;
 
-	private:
-		void InitWindow();
-		void InitVulkan();
-		void MainLoop();
-		void CleanUp();
+        //Sync objects -> Waiting them to make operation... cf doc vulkan
+        std::vector<VkSemaphore> m_imageAvailableSemaphores;
+        std::vector<VkSemaphore> m_renderFinishedSemaphores;
+        std::vector<VkFence> m_inFlightFences;
 
-		void CreateInstance();
-		void SetupDebugMessenger();
-		void CreateSurface();
-		void PickPhysicalDevice();
-		void CreateLogicalDevice();
-		void CreateSwapChain();
-		void CreateImageViews();
-		void CreateRenderPass();
-		void CreateGraphicsPipeline();
-		void CreateFramebuffers();
-		void CreateCommandPool();
-		void CreateCommandBuffer();
-		void CreateSyncObjects();
+    private:
+        void InitWindow();
+        void InitVulkan();
+        void MainLoop();
+        void CleanUp();
 
-		void DrawFrame();
+        void CreateInstance();
+        void SetupDebugMessenger();
+        void CreateSurface();
+        void PickPhysicalDevice();
+        void CreateLogicalDevice();
+        void CreateSwapChain();
+        void CreateImageViews();
+        void CreateRenderPass();
+        void CreateGraphicsPipeline();
+        void CreateFramebuffers();
+        void CreateCommandPool();
+        void CreateCommandBuffer();
+        void CreateSyncObjects();
 
-		bool CheckDeviceExtensionSupport(VkPhysicalDevice device);
-		int RateDeviceSuitability(VkPhysicalDevice device);
-		bool CheckValidationLayerSupport();
-		std::vector<const char*> GetRequiredExtensions();
-		void PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
-		QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
-		SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device);
-		VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
-		VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
-		VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
-		VkShaderModule CreateShaderModule(const std::vector<char>& code);
-		void RecordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+        void DrawFrame();
 
-		static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
-			VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-			VkDebugUtilsMessageTypeFlagsEXT messageType,
-			const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-			void* pUserData);
-		static std::vector<char> ReadFile(const std::string& filename);
-	};
+        bool CheckDeviceExtensionSupport(VkPhysicalDevice device);
+        int RateDeviceSuitability(VkPhysicalDevice device);
+        bool CheckValidationLayerSupport();
+        std::vector<const char*> GetRequiredExtensions();
+        void PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+        QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
+        SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device);
+        VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+        VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+        VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+        VkShaderModule CreateShaderModule(const std::vector<char>& code);
+        void RecordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+
+        static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+            VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+            VkDebugUtilsMessageTypeFlagsEXT messageType,
+            const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+            void* pUserData);
+        static std::vector<char> ReadFile(const std::string& filename);
+    };
 }
