f83b9b558a48eb1b52b42d51458641c1c8d65154
Nazio_LT <zoupeurletroll@gmail.com>
2025-02-16 18:25:03 +0100

    :green_heart: Auto compile shader fix

---
+++ core/narc_engine/shaders/compile_shaders.bat
@@ -0,0 +1,4 @@
+
+C:/VulkanSDK/1.4.304.1/Bin/glslc.exe shader.vert -o shader_vert.spv
+C:/VulkanSDK/1.4.304.1/Bin/glslc.exe shader-eee.frag -o shader-eee_frag.spv
+C:/VulkanSDK/1.4.304.1/Bin/glslc.exe shader.frag -o shader_frag.spv
