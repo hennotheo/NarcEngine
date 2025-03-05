ecf51674df665de82b3efb1fb02492bdf780ebda
Nazio_LT <zoupeurletroll@gmail.com>
2025-03-05 23:55:49 +0100

    :construction_worker: Update Cmake build system

--- core/narcio/src/FileReader.cpp
+++ core/narcio/src/FileReader.cpp
@@ -3,7 +3,7 @@
 #include <NarcLog.h>
 #include <NarcMath.h>
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
+#include <stb_image.h>
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
 
