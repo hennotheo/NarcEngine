5366bb47d083cb8bbca5163e37655b7c029ca676
hennotheo <zoupeurletroll@gmail.com>
2025-04-23 11:47:13 +0200

    :green_heart: Config tests

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -5,15 +5,3 @@ function(narc_blob_source_files)
     set(CPP_FILES ${CPP_FILES} PARENT_SCOPE)
     set(HPP_FILES ${HPP_FILES} PARENT_SCOPE)
 endfunction()
-
-function(narc_auto_setup_tests TARGET_NAME)
-    add_executable(Tests_${TARGET_NAME} tests/test_main.cpp)
-
-    target_link_libraries(Tests_${TARGET_NAME} PRIVATE
-        gtest gtest_main
-        gmock gmock_main
-        ${TARGET_NAME}
-    )
-    gtest_discover_tests(Tests_${TARGET_NAME}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/tests)
-endfunction()
---
+++ cmake/tests.cmake
@@ -0,0 +1,29 @@
+option(ENABLE_TESTS "Enable Tests" ON)
+
+function(narc_auto_glob_tests)
+    file(GLOB_RECURSE TESTS_CPP_FILES ${CMAKE_CURRENT_SOURCE_DIR}/tests/*.cpp)
+    file(GLOB_RECURSE TESTS_HPP_FILES ${CMAKE_CURRENT_SOURCE_DIR}/tests/*.h)
+
+    set(TESTS_CPP_FILES ${TESTS_CPP_FILES} PARENT_SCOPE)
+    set(TESTS_CPP_FILES ${TESTS_CPP_FILES} PARENT_SCOPE)
+endfunction()
+
+
+function(narc_auto_setup_tests TARGET_NAME)
+    add_executable(Tests_${TARGET_NAME} tests/test_main.cpp)
+
+    target_link_libraries(Tests_${TARGET_NAME} PRIVATE
+        gtest gtest_main
+        gmock gmock_main
+        ${TARGET_NAME}
+    )
+    gtest_discover_tests(Tests_${TARGET_NAME}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/tests)
+endfunction()
+
+if(ENABLE_TESTS)
+
+add_compile_definitions(NARC_TEST_BUILD)
+enable_testing()
+
+endif()
