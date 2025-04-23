169be9dbfce55afe453152d4f665401bcee353a1
hennotheo <zoupeurletroll@gmail.com>
2025-04-23 14:39:29 +0200

    :green_heart: Fixed G++

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,4 +1,4 @@
-option(ENABLE_TESTS "Enable Tests" ON)
+option(ENABLE_TESTS "Enable Tests" OFF)
 
 function(narc_auto_glob_tests)
     file(GLOB_RECURSE TESTS_CPP_FILES ${CMAKE_CURRENT_SOURCE_DIR}/tests/*.cpp)
@@ -8,8 +8,11 @@ function(narc_auto_glob_tests)
     set(TESTS_CPP_FILES ${TESTS_CPP_FILES} PARENT_SCOPE)
 endfunction()
 
-
 function(narc_auto_setup_tests TARGET_NAME)
+    if(NOT ENABLE_TESTS)
+        return()
+    endif()
+
     add_executable(Tests_${TARGET_NAME} tests/test_main.cpp)
 
     target_link_libraries(Tests_${TARGET_NAME} PRIVATE
@@ -23,7 +26,7 @@ endfunction()
 
 if(ENABLE_TESTS)
 
-add_compile_definitions(NARC_TEST_BUILD)
-enable_testing()
+    add_compile_definitions(NARC_TEST_BUILD)
+    enable_testing()
 
 endif()
