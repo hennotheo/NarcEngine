d9c0445d6020a036b07fd6daeb3bfd0e96e34cd6
hennotheo <theo.henno21@gmail.com>
2025-05-22 20:25:50 +0200

    :white_check_mark: Test for base ContextRhi

--- rendering/rhi/tests/test_main.cpp
+++ rendering/rhi/tests/test_main.cpp
@@ -6,9 +6,43 @@
 
 #include <gmock/gmock.h>
 
-TEST(NarcRhi, Test)
+#include "rhi/ContextRhi.h"
+
+using namespace narc_engine;
+
+class RhiTest : public ::testing::Test
 {
-    EXPECT_EQ(1, 1);
+protected:
+    void SetUp() override
+    {
+        // Setup code here
+    }
+
+    void TearDown() override
+    {
+        // Cleanup code here
+    }
+
+    virtual RendererApiType getTestedApi() = 0;
+};
+
+class RhiTestVulkan : public RhiTest
+{
+protected:
+    RendererApiType getTestedApi() override { return RendererApiType::Vulkan; }
+};
+
+TEST_F(RhiTestVulkan, ContextRhi_Creation)
+{
+    ContextRhiPtr context = createContextRhi(getTestedApi());
+    ASSERT_NE(context, nullptr) << "Failed to create ContextRhi for Vulkan API";
+}
+
+TEST_F(RhiTestVulkan, ContextRhi_InitShutdown)
+{
+    ContextRhiPtr context = createContextRhi(getTestedApi());
+    EXPECT_NO_THROW(context->init()) << "ContextRhi initialization threw an exception";
+    EXPECT_NO_THROW(context->shutdown()) << "ContextRhi shutdown threw an exception";
 }
 
 int main(int argc, char** argv)
