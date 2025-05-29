0c64d9b59ab24e06a52e25c97136a23bf5085e3d
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2025-05-30 01:19:04 +0200

    :sparkles: Add Sync Rhi classes files

---
+++ rendering/rhi/include/rhi/sync/FenceRhi.h
@@ -0,0 +1,13 @@
+//
+// Created by theoh on 30/05/2025.
+//
+
+#pragma once
+
+namespace narc_engine {
+
+class FenceRhi {
+
+};
+
+} // narc_engine
---
+++ rendering/rhi/src/backend_vulkan/CommandQueueVulkan.cpp
@@ -0,0 +1,9 @@
+//
+// Created by theoh on 30/05/2025.
+//
+
+#include "CommandQueueVulkan.h"
+
+namespace narc_engine
+{
+} // narc_engine
