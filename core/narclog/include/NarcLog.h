e6f4cd1d478da786b6dd865afb6f0c5968874e23
hennotheo <theo.henno21@gmail.com>
2025-10-09 01:37:43 +0200

    :sparkles: Add Exception base for engine

--- core/narclog/include/NarcLog.h
+++ core/narclog/include/NarcLog.h
@@ -6,6 +6,8 @@
 
 #include "keywords/LogLevel.h"
 
+#include "exceptions/NarcEngineException.h"
+
 #define NARC_LOG_DEBUG(...) narc_log::log(DEBUG, __VA_ARGS__);
 #define NARC_LOG_INFO(...) narc_log::log(INFO, __VA_ARGS__)
 #define NARC_LOG_WARNING(...) narc_log::log(WARNING, __VA_ARGS__)
---
+++ core/narclog/src/exceptions/NarcEngineException.cpp
@@ -0,0 +1,9 @@
+//
+// Created by theohenno on 10/9/25.
+//
+
+#include "exceptions/NarcEngineException.h"
+
+namespace narc_log {
+
+} // narc_log
