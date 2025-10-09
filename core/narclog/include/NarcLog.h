4aa8e070fbd7910f7d5456c86ca6edf408841408
hennotheo <theo.henno21@gmail.com>
2025-10-10 00:31:37 +0200

    :goal_net: Adding NotimplementedError

--- core/narclog/include/NarcLog.h
+++ core/narclog/include/NarcLog.h
@@ -4,12 +4,14 @@
 #include "pch.h"
 
 #include "exceptions/LogicError.h"
+#include "exceptions/NotImplementedError.h"
 #include "exceptions/RuntimeError.h"
 
 #include "keywords/LogLevel.h"
 
-#define NARC_ERROR_LOGIC(...) throw narc_log::LogicError(__VA_ARGS__)
-#define NARC_ERROR_RUNTIME(...) throw narc_log::RuntimeError(__VA_ARGS__)
+#define NARC_ERROR_LOGIC(...) throw narc_log::LogicError(narc_log::format(__VA_ARGS__));
+#define NARC_ERROR_RUNTIME(...) throw narc_log::RuntimeError(narc_log::format(__VA_ARGS__));
+#define NARC_ERROR_NOT_IMPLEMENTED(...) throw narc_log::NotImplementedError(narc_log::format(__VA_ARGS__));
 
 #define NARC_LOG_DEBUG(...) narc_log::log(DEBUG, __VA_ARGS__)
 #define NARC_LOG_INFO(...) narc_log::log(INFO, __VA_ARGS__)
@@ -21,23 +23,24 @@ namespace narc_log {
     [[maybe_unused]] NARC_LOG_API void init_signal_handling();
 
     template<typename... Args>
-    [[maybe_unused]] NARC_LOG_API void log(const LogLevel &level, spdlog::format_string_t<Args...> fmt, Args &&... args) {
-        switch (level) {
-            case DEBUG:
-                spdlog::debug(fmt, std::forward<Args>(args)...);
-                break;
-            case INFO:
-                spdlog::info(fmt, std::forward<Args>(args)...);
-                break;
-            case WARNING:
-                spdlog::warn(fmt, std::forward<Args>(args)...);
-                break;
-            case ERROR:
-                spdlog::error(fmt, std::forward<Args>(args)...);
-                break;
-            case FATAL:
-                spdlog::critical(fmt, std::forward<Args>(args)...);
-                break;
+    [[maybe_unused]] NARC_LOG_API void log(const LogLevel& level, spdlog::format_string_t<Args...> fmt, Args&&... args)
+    {
+        switch (level)
+        {
+            case DEBUG: spdlog::debug(fmt, std::forward<Args>(args)...); break;
+            case INFO: spdlog::info(fmt, std::forward<Args>(args)...); break;
+            case WARNING: spdlog::warn(fmt, std::forward<Args>(args)...); break;
+            case ERROR: spdlog::error(fmt, std::forward<Args>(args)...); break;
+            case FATAL: spdlog::critical(fmt, std::forward<Args>(args)...); break;
         }
     }
-}
+
+    template<typename... Args>
+    [[maybe_unused]] NARC_LOG_API std::string format(spdlog::format_string_t<Args...> fmt, Args&&... args)
+    {
+        return fmt::format(fmt, std::forward<Args>(args)...);
+    }
+
+    [[maybe_unused]] NARC_LOG_API inline std::string format(const std::string& message) { return message; }
+    [[maybe_unused]] NARC_LOG_API inline std::string format(const char* message) { return message; }
+} // namespace narc_log
--- core/narclog/include/exceptions/NotImplementedError.h
+++ core/narclog/include/exceptions/NotImplementedError.h
@@ -9,11 +9,11 @@ namespace narc_log {
      * Exception class for handling logic errors specific to the NarcEngine.
      * Inherits from std::logic_error to provide standard exception functionality.
      */
-    class LogicError : public std::logic_error
+    class NotImplementedError : public std::logic_error
     {
     public:
-        explicit LogicError(const std::string& message) : std::logic_error(message) {}
+        explicit NotImplementedError(const std::string& message) : std::logic_error(message) {}
 
-        ~LogicError() noexcept override = default;
+        ~NotImplementedError() noexcept override = default;
     };
 } // namespace narc_log
