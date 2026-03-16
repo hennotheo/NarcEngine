806b4038a6775ae91e71dc90ecdff6eeb881c6f2
theohenno <theo.henno21@gmail.com>
2026-03-16 23:10:47 +0100

    :sparkles: Add PI
    Signed-off-by: theohenno <theo.henno21@gmail.com>

--- core/narc_math/include/NarcMath.h
+++ core/narc_math/include/NarcMath.h
@@ -12,10 +12,11 @@
 #include "vectors/Vec2.h"
 #include "vectors/Vec2Int.h"
 
+#include "Extent.h"
+
 #define NARC_KB_SIZEOF(obj) (static_cast<double>(sizeof(obj)) / 1024.0)
 
 namespace narc_math {
     constexpr float EPSILON_F = 1e-6f;
+    constexpr float PI_F = 3.14159265358979323846f;
 }
-
-#include "Extent.h"
